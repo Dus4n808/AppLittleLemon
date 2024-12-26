6167227fb7e7b054b7a035130e19c3eb28864478
Dusan <dusanfama@gmail.com>
2024-12-26 18:48:10 +0100

    my app

---
+++ Restaurant/RestaurantTests/RestaurantTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantTests.swift
+//  RestaurantTests
+//
+//  Created by Dusan Fama on 26.12.2024.
+//
+
+import Testing
+@testable import Restaurant
+
+struct RestaurantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
